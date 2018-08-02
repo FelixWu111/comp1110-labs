1251cbd59deeb1b1e08e3f37e22e628889a29b7f
Yu Wu <u6250866@anu.edu.au>
2018-08-02 14:44:50 +1000

    doing lab exercises

---
+++ src/comp1110/lab2/Extension.java
@@ -0,0 +1,23 @@
+package comp1110.lab2;
+
+public class Extension {
+    public static void main(String[] args) {
+        int[] array=new int[] {11,22,33,44,5,66,77,88,99,100};
+        int a =0;
+        for (int i=0;i<array.length;i++){
+            if (array[i]==5) {
+                a=5;
+                break;
+
+            }else{a+=array[i];}
+        }
+        int counter =0;
+        System.out.println(a);
+        for (int j=1;j<=100;j++){
+            if (j%3==0){
+                continue;
+            }else{counter=counter+j;}
+        }
+        System.out.println(counter);
+    }
+}
